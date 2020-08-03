a4305756480917fa8b8cc912d0b7f408f112b021
mauricioabisay <mauricioabisay.lopez@gmail.com>
2020-08-03 08:25:37 -0500

    Add logic for shorten URL generation.

--- src/main/java/javaSchool/Task000/URLShortenerService.java
+++ src/main/java/javaSchool/Task000/URLShortenerService.java
@@ -1,5 +1,7 @@
 package javaSchool.Task000;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class URLShortenerService {
 	private final static String URL_GOOGLE_PATTERN = "^.*(google).*$";
 	private final static String URL_YAHOO_PATTERN  = "^.*(yahoo).*$";
@@ -11,6 +13,16 @@ public class URLShortenerService {
 	
 	public static String shortURL(String url, int count) {
 		StringBuilder urlShorten = new StringBuilder();
+		if (url.matches(URL_GOOGLE_PATTERN)) {
+			urlShorten.append(RandomStringUtils.randomAlphabetic(5));
+		} else if (url.matches(URL_YAHOO_PATTERN)) {
+			urlShorten.append(RandomStringUtils.randomAlphanumeric(7));
+		} else if (count > 0) {
+			urlShorten.append(url.toLowerCase().replaceAll(URL_DEFAULT_REMOVE, ""));
+			urlShorten.append(count);
+		} else {
+			urlShorten.append(url.toLowerCase().replaceAll(URL_DEFAULT_REMOVE, ""));
+		}
 		return urlShorten.toString();
 	}
 }
